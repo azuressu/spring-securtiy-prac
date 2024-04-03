097a45eae9c6d613244bb87823f02f366d2d79ea
azuressu <martina0130@naver.com>
2024-04-03 16:15:20 +0900

    :sparkles: create userRepository

---
+++ src/main/java/pac/security/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package pac.security.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import pac.security.entity.UserEntity;
+
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+
+
+
+
+}
