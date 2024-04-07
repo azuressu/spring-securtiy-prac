bab38d0129359caf1a9b60c262c2108cbc7a4e74
azuressu <martina0130@naver.com>
2024-04-07 21:18:25 +0900

    :sparkles: add login filter

---
+++ src/main/java/pac/security/filter/LoginFilter.java
@@ -0,0 +1,52 @@
+package pac.security.filter;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.NoArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationFilter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+public class LoginFilter extends UsernamePasswordAuthenticationFilter {
+
+    private final AuthenticationManager authenticationManager;
+
+    public LoginFilter(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) {
+
+        // 클라이언트 요청에서 username, password 추출하기
+        String username = obtainUsername(request);
+        String password = obtainPassword(request);
+
+        // Spring Security에서 username과 password를 검증하기 위해서는 token에 담아주어야 함
+        UsernamePasswordAuthenticationToken authenticationToken
+                = new UsernamePasswordAuthenticationToken(username, password, null);
+
+        // token에 담은 검증을 위한 AuthenticationManager로 전달
+        return authenticationManager.authenticate(authenticationToken);
+    }
+
+    // 로그인 성공 시 실행하는 메소드 (여기서 JWT 발급)
+    /* 로그인 성공 시 successfulAuthentication() 메소드를 통해 JWT를 응답해야 함.
+    *  따라서 JWT 응답 구문 작성이 필요한데, JWT 발급 클래스를 아직 생성하지 않았으므로
+    *  다음 시간에 DB 기반 회원 검증 구현 후, JWT 발급 및 검증을 진행하는 클래스 생성 예정*/
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response) {
+
+    }
+
+    // 로그인 실패 시 실행하는 메소드
+    @Override
+    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response) {
+
+    }
+
+
+
+}
